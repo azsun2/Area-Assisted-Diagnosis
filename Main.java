116e3349905880f372d7e3cf5009ad2d06f04bf5
JoLBree <JoLBree@users.noreply.github.com>
2016-02-20 13:21:36 -0600

    multi-word disease, multi-diesease

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package test2;
 
 //import test.SQLException;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -13,76 +14,48 @@ import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args) {
-		doSmth("Pain knee", "07928");
+		//parseNamesAndRun("Pain knee", "07928");
+		String[] names = {"Pain knee", "Pain Back"};
+		int[] counts = countMultiNamesOneZip(names, "07928");
+		for(int i : counts)
+		{
+			System.out.println(i);
+		}
+		Integer[] i = {1, 3281};
+		//getCount(i);
 		//getIds("pain", "07928");
 		//System.out.println(getCount("pain", "07asdf28"));
 	}
 
-	/*
-	 * Limitations:
-	 * Disease currently limited to one-word diseases only
-	 * No input filters, simply returns 0 if bullshit (which i guess makes sense)
-	 * 
-	 * Steps for java-mysql hookup:
-	 * Following the comment with 244 +1s here: http://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database
-	 * If using eclipse, this tells you how to add it to the class path: http://www.wikihow.com/Add-JARs-to-Project-Build-Paths-in-Eclipse-(Java)
-	 * Commented out code below to test connection
-	 * 
-	 */
-	public static int getCount(String disease, String zipcode){
-		Connection con = null;
-		PreparedStatement stmt = null;
-		try {
-			con = DriverManager.getConnection( // apologies for hardcoding the password
-					"jdbc:mysql://linktothepast.cm0t4637eidl.us-east-1.rds.amazonaws.com:3306/portal","triforce","turtlerock");
-			stmt = con.prepareStatement("select count(*) as total from ( "
-					+ " select s1.requestlog_id  from ( "
-					+ "  select requestlog_id  "
-					+ " from requestlog_terms "
-					+ "  where word=?  "
-					+ " limit 1000  )"
-					+ " s1 ) ids "
-					+ "join requestlog on ids.requestlog_id=requestlog.id "
-					+ "where requestlog.zip = ?; ");
-			stmt.setString(1, disease);
-			stmt.setString(2, zipcode);
-			ResultSet rs = stmt.executeQuery();
-			if(rs.next()){
-				return (rs.getInt("total"));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static int[] countMultiNamesOneZip(String[] names, String zipcode)
+	{
+		int[] countArray = new int[names.length];
+		int i = 0;
+		for(String name : names)
+		{
+			countArray[i] = parseNamesAndRun(name, zipcode);
+			i++;
 		}
-		finally {
-			try {
-				if (stmt != null) { stmt.close(); }
-			}
-			catch (Exception e) {
-				System.out.println("Error: Query prep failed");
-			}
-			try {
-				if (con != null) { con.close(); }
-			}
-			catch (Exception e) {
-				System.out.println("Error: Connection failed");
-			}
-		}
-		return 0;
+		return countArray;
+		
 	}
-	public static void doSmth (String fullName, String zipcode)
+	
+	public static int parseNamesAndRun (String fullName, String zipcode)
 	{
 		String[] splitString = fullName.split(" ");		
 		HashMap<Integer, Integer> ids = new HashMap<Integer, Integer>();
+		int length = splitString.length;
 		for (String name : splitString)
 		{
 			ids = getIds(name, zipcode, ids);
 		}
-		System.out.println(ids);
+		int count = 0;
+		for (Integer id : ids.keySet()){
+			if (ids.get(id) == length){
+				count++;
+			}}
+		return count;
 	}
-	
-	
-	
 	
 	public static HashMap<Integer, Integer> getIds (String disease, String zipcode, HashMap<Integer, Integer> ids){
 		
@@ -106,15 +79,12 @@ public class Main {
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()){
 				Integer tempId = rs.getInt("id");
-				if (ids.containsKey(tempId))
-				{
+				if (ids.containsKey(tempId)){
 					Integer tempValue = (Integer) ids.get(tempId);
 					tempValue++;
 					ids.put(tempId, tempValue);
-					
 				}
-				else
-				{
+				else{
 					ids.put(tempId, 1);
 				}
 			}
@@ -139,9 +109,7 @@ public class Main {
 		}
 		System.out.println(ids);
 		return ids;
-		//return {0,0};
-	}
-	
+	}	
 }
 
 
@@ -155,4 +123,94 @@ public class Main {
 //    System.out.println("Database connected!");
 //} catch (SQLException e) {
 //    throw new IllegalStateException("Cannot connect the database!", e);
+//}
+
+/*
+ * Limitations:
+ * Disease currently limited to one-word diseases only
+ * No input filters, simply returns 0 if bullshit (which i guess makes sense)
+ * 
+ * Steps for java-mysql hookup:
+ * Following the comment with 244 +1s here: http://stackoverflow.com/questions/2839321/connect-java-to-a-mysql-database
+ * If using eclipse, this tells you how to add it to the class path: http://www.wikihow.com/Add-JARs-to-Project-Build-Paths-in-Eclipse-(Java)
+ * Commented out code below to test connection
+ * 
+ */
+//public static int getCount(String disease, String zipcode){
+//	Connection con = null;
+//	PreparedStatement stmt = null;
+//	try {
+//		con = DriverManager.getConnection( // apologies for hardcoding the password
+//				"jdbc:mysql://linktothepast.cm0t4637eidl.us-east-1.rds.amazonaws.com:3306/portal","triforce","turtlerock");
+//		stmt = con.prepareStatement("select count(*) as total from ( "
+//				+ " select s1.requestlog_id  from ( "
+//				+ "  select requestlog_id  "
+//				+ " from requestlog_terms "
+//				+ "  where word=?  "
+//				+ " limit 1000  )"
+//				+ " s1 ) ids "
+//				+ "join requestlog on ids.requestlog_id=requestlog.id "
+//				+ "where requestlog.zip = ?; ");
+//		stmt.setString(1, disease);
+//		stmt.setString(2, zipcode);
+//		ResultSet rs = stmt.executeQuery();
+//		if(rs.next()){
+//			return (rs.getInt("total"));
+//		}
+//	} catch (SQLException e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//	finally {
+//		try {
+//			if (stmt != null) { stmt.close(); }
+//		}
+//		catch (Exception e) {
+//			System.out.println("Error: Query prep failed");
+//		}
+//		try {
+//			if (con != null) { con.close(); }
+//		}
+//		catch (Exception e) {
+//			System.out.println("Error: Connection failed");
+//		}
+//	}
+//	return 0;
+//}
+
+//public static int getCount(Integer[] commonIds){ // change param type 
+//Object[] objIds = (Object[]) commonIds; // figure out casting later
+//Connection con = null;
+//PreparedStatement stmt = null;
+//
+//try {
+//	con = DriverManager.getConnection( // apologies for hardcoding the password
+//			"jdbc:mysql://linktothepast.cm0t4637eidl.us-east-1.rds.amazonaws.com:3306/portal","triforce","turtlerock");
+//	stmt = con.prepareStatement("select count(*) as total from requestlog where id in ?; ");
+//	Array array = con.createArrayOf("INT", objIds );
+//	stmt.setArray(1, array);
+//	ResultSet rs = stmt.executeQuery();
+//	if(rs.next()){
+//		return rs.getInt("total");
+//	}
+//} catch (SQLException e) {
+//	// TODO Auto-generated catch block
+//	e.printStackTrace();
+//}
+//finally {
+//	try {
+//		if (stmt != null) { stmt.close(); }
+//	}
+//	catch (Exception e) {
+//		System.out.println("Error: Query prep failed");
+//	}
+//	try {
+//		if (con != null) { con.close(); }
+//	}
+//	catch (Exception e) {
+//		System.out.println("Error: Connection failed");
+//	}
+//}
+//return 0;
+//
 //}
